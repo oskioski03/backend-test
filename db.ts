d7f94603c79a5ac5e0425d080795ff2f85a1f7db
oskioski03 <oskioski03@wp.pl>
2024-11-15 22:24:47 +0100

    asddasasd

--- db.ts
+++ db.ts
@@ -1,10 +1,10 @@
 import {Pool} from 'pg'
 
 export const pool = new Pool({
-  user: 'anon',
+  user: 'smart',
   password: 'admin',
   host: 'postgres',
   port: 5432,
-  database: 'backend_test_1'
+  database: 'backend_test_smart'
 });
 
